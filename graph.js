b25a66ced68065f07c5f9f591205daefe7e720b1
Bovard Tiberi <bovard.tiberi@gmail.com>
2015-10-09 17:59:05 -0700

    Added attribution

--- graph.js
+++ graph.js
@@ -1,5 +1,7 @@
 var steps = [19938, 4964, 3231, 3327, 5234, 26117, 11445, 4140, 12274, 10078, 9759, 3993, 3896, 11564, 3126, 7345, 4482, 9230, 25756, 11367, 13453, 14021, 8734, 9012, 12012, 25012, 11781, 10127, 6462, 7895];
 
+// followed tutorial from http://bl.ocks.org/mbostock/7322386#index.html
+
 // define the scale
 var x = d3.scale.linear().domain([0, d3.max(steps)]).range([0, 820]);
 
